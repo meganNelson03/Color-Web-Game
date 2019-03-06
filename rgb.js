d2f5a718e7911cecf214d4363abf7e4d33032dfd
Megan Nelson <menelson@vassar.edu>
2019-03-05 22:30:11 -0500

    refactored

--- rgb.js
+++ rgb.js
@@ -1,64 +1,42 @@
 var numOfSquares = 6;
-var colors = generateRandomColors(numOfSquares);
+var colors = [];
+var winColor;
 var squares = document.querySelectorAll(".square");
-var winColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var easy = document.querySelector("#easy");
-var hard = document.querySelector("#hard");
+var modes = document.querySelectorAll(".mode");
 
 colorDisplay.textContent = winColor;
 
-easy.addEventListener("click", function() {
-	easy.classList.add("selected");
-	hard.classList.remove("selected");
-	numOfSquares = 3;
-	colors = generateRandomColors(numOfSquares);
-	winColor = pickColor();
+init();
 
-	colorDisplay.textContent = winColor;
+function init() {
+	setUpModes(); 
+	squareSetUp();
+	reset();
+}
+
+function setUpModes() {
+	for (var i=0; i<modes.length; i++){
+		modes[i].addEventListener("click", function() {
+			modes[0].classList.remove("selected");
+			modes[1].classList.remove("selected");
+
+			this.classList.add("selected");
+
+			this.textContent==="Easy" ? numOfSquares = 3 : numOfSquares = 6;
+
+			reset();
+
+		});
+	}
+}
+
+function squareSetUp() {
 	for (var i=0; i<squares.length; i++){
-		if (colors[i]){
-			squares[i].style.background = colors[i];
-		} else {
-			squares[i].style.display = "none";
-		}
-	}
-	h1.style.background = "steelblue";
-});
-
-hard.addEventListener("click", function() {
-	hard.classList.add("selected");
-	easy.classList.remove("selected");
-	numOfSquares = 6;
-	colors = generateRandomColors(numOfSquares);
-	winColor = pickColor();
-	colorDisplay.textContent = winColor;
-	for (var i=0;i<squares.length;i++){
-		
-		squares[i].style.background = colors[i];
-		squares[i].style.display = "initial";
-	}
-	h1.style.background = "steelblue";
-});
-
-resetButton.addEventListener("click", function() {
-	
-	colors = generateRandomColors(numOfSquares);
-	winColor = pickColor();
-	colorDisplay.textContent = winColor;
-	
-	for (var i=0; i<squares.length;i++){
-		squares[i].style.background = colors[i];
-	}
-	h1.style.background = "steelblue";
-});
-
-for (var i=0; i<squares.length; i++){
-	squares[i].style.backgroundColor = colors[i];
-	squares[i].addEventListener("click", function(){
+		squares[i].addEventListener("click", function(){
 		var clickedColor = this.style.backgroundColor;
 		if(clickedColor === winColor){
 			messageDisplay.textContent = "Correct";
@@ -71,8 +49,30 @@ for (var i=0; i<squares.length; i++){
 			h1.style.background = "steelbue";
 		}	
 	});
+	}
 }
 
+function reset() {
+	colors = generateRandomColors(numOfSquares);
+	winColor = pickColor();
+	colorDisplay.textContent = winColor;
+	resetButton.textContent = "New Colors";
+	messageDisplay.textContent = "";
+
+	for(var i=0; i<squares.length; i++){
+		if (colors[i]){
+			squares[i].style.display = "block";
+			squares[i].style.background = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+	h1.style.background = "steelblue";
+}
+
+resetButton.addEventListener("click", function() {
+	reset();
+});
 
 function changeColors(color){
 	for (var i=0; i<squares.length; i++){
